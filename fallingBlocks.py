845fedb522ac1ca22b9705d4284521e693bc3e23
Daniel Gauvin <danielgauvin01@gmail.com>
2020-07-07 13:42:13 -0400

    multiple enemy blocks falling

--- fallingBlocks.py
+++ fallingBlocks.py
@@ -16,6 +16,7 @@ player_pos = [width / 2, height - player_size * 2]
 
 enemy_size = 50
 enemy_pos = [random.randrange(width - enemy_size), 0]
+enemy_list = [enemy_pos]
 
 falling_speed = 10
 
@@ -39,10 +40,38 @@ def detect_collision(player_pos, enemy_pos):
     return False
 
 
+def drop_blocks(enemy_list):
+    delay = random.random()
+    if len(enemy_list) < 10 and delay < 0.3:
+        x_pos = random.randrange(width - enemy_size)
+        y_pos = 0
+        enemy_list.append([x_pos, y_pos])
+
+
+def draw_enemies(enemy_list):
+    for enemy_pos in enemy_list:
+        # define enemy block
+        pygame.draw.rect(screen, BLUE, (enemy_pos[0], enemy_pos[1], enemy_size, enemy_size))
+
+
+def update_enemy_position(enemy_list):
+    for index, enemy_pos in enumerate(enemy_list):
+        if enemy_pos[1] >= 0 and enemy_pos[1] < height:
+            enemy_pos[1] += falling_speed
+        else:
+            enemy_list.pop(index)
+
+def collision_check(enemy_list, player_position):
+
+    for enemy_pos in enemy_list:
+        if detect_collision(enemy_pos, player_pos):
+            return True
+    return False
+
+
 while not game_over:
 
     for event in pygame.event.get():
-        print(event)
 
         if event.type == pygame.QUIT:
             sys.exit()
@@ -67,19 +96,16 @@ while not game_over:
     # define the background color after every event
     screen.fill(BACKGROUND_COLOR)
 
-    # falling blocks position
-    if enemy_pos[1] >= 0 and enemy_pos[1] < height:
-        enemy_pos[1] += falling_speed
-    else:
-        enemy_pos[0] = random.randrange(width - enemy_size)
-        enemy_pos[1] = 0
-
     if detect_collision(player_pos, enemy_pos):
         game_over = True
+        break
 
-    # define enemy block
-    pygame.draw.rect(screen, BLUE, (enemy_pos[0], enemy_pos[1], enemy_size, enemy_size))
+    drop_blocks(enemy_list)
+    update_enemy_position(enemy_list)
+    if collision_check(enemy_list, player_pos):
+        game_over = True
 
+    draw_enemies(enemy_list)
     # defnie player block
     pygame.draw.rect(screen, RED, (player_pos[0], player_pos[1], player_size, player_size))
 
