c2707a30589fb4c173d86661b1b5340168a288ef
Daniel Gauvin <danielgauvin01@gmail.com>
2020-07-07 13:08:11 -0400

    single block falling works

--- fallingBlocks.py
+++ fallingBlocks.py
@@ -1,5 +1,6 @@
 import pygame
 import sys
+import random
 
 pygame.init()
 
@@ -8,14 +9,30 @@ height = 600
 
 RED = (255, 0, 0)
 BACKGROUND_COLOR = (0, 0, 0)
+BLUE = (0, 0, 255)
 
 player_size = 50
-player_pos = [width / 2, height - 2 * player_size]
+player_pos = [width / 2, height - player_size * 2]
 
+enemy_size = 50
+enemy_pos = [random.randrange(width - enemy_size), 0]
+
+falling_speed = 10
+
+clock = pygame.time.Clock()
 
 screen = pygame.display.set_mode((width, height))
 
 game_over = False
+
+
+def detect_collision(player_pos, enemy_pos):
+    p_x = player_pos[0]
+    p_y = player_pos[1]
+
+    e_x = enemy_pos[0]
+    e_y = enemy_pos[1]
+
 
 while not game_over:
 
@@ -25,6 +42,7 @@ while not game_over:
         if event.type == pygame.QUIT:
             sys.exit()
 
+        # move the player
         if event.type == pygame.KEYDOWN:
 
             x = player_pos[0]
@@ -41,8 +59,21 @@ while not game_over:
 
             player_pos = [x, y]
 
+    # define the background color after every event
     screen.fill(BACKGROUND_COLOR)
-    pygame.draw.rect(
-        screen, RED, (player_pos[0], player_pos[1], player_size, player_size))
 
+    # falling blocks position
+    if enemy_pos[1] >= 0 and enemy_pos[1] < height:
+        enemy_pos[1] += falling_speed
+    else:
+        enemy_pos[0] = random.randrange(width - enemy_size)
+        enemy_pos[1] = 0
+
+    # define enemy block
+    pygame.draw.rect(screen, BLUE, (enemy_pos[0], enemy_pos[1], enemy_size, enemy_size))
+
+    # defnie player block
+    pygame.draw.rect(screen, RED, (player_pos[0], player_pos[1], player_size, player_size))
+
+    clock.tick(30)
     pygame.display.update()
