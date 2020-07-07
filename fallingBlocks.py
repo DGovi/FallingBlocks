7c9505cad6b4a8163583d5bb04ea96ca099ccf43
Daniel Gauvin <danielgauvin01@gmail.com>
2020-07-07 13:17:16 -0400

    collision detection

--- fallingBlocks.py
+++ fallingBlocks.py
@@ -33,6 +33,11 @@ def detect_collision(player_pos, enemy_pos):
     e_x = enemy_pos[0]
     e_y = enemy_pos[1]
 
+    if (e_x >= p_x and e_x < (p_x + player_size)) or ((p_x >= e_x) and p_x < (e_x + enemy_size)):
+        if (e_y >= p_y) and e_y < (p_y + player_size) or (p_y >= e_y) and p_y < (e_y + enemy_size):
+            return True
+    return False
+
 
 while not game_over:
 
@@ -68,6 +73,9 @@ while not game_over:
     else:
         enemy_pos[0] = random.randrange(width - enemy_size)
         enemy_pos[1] = 0
+
+    if detect_collision(player_pos, enemy_pos):
+        game_over = True
 
     # define enemy block
     pygame.draw.rect(screen, BLUE, (enemy_pos[0], enemy_pos[1], enemy_size, enemy_size))
